2d754876bd86e75df8eb86a6fed1eb7ac3dae8db
Theo Ojochegbe <symplytheo@gmail.com>
2024-02-25 12:11:42 +0100

    feat: add time

--- src/utilities/fomartDate.js
+++ src/utilities/fomartDate.js
@@ -1,22 +1,30 @@
 /**
  * Formats a date string into dd-mm-yyyy ( mm is month's short name e.g. Jan, Feb) and short month plus date.
  * @param {String} dateString - The date in string to convert.
- * @returns {<string>} - An object with date (e.g. 01-Jan-2000) and monthDate (e.g. Jan 20) property.
+ * @returns {Object} - An object with date (e.g. 01-Jan-2000), monthDate (e.g. Jan 20), time (e.g. 12:34 PM),
+ *                    and other properties such as year, monthShort, day, hours, minutes, am/pm.
  */
-
 function formatDate(dateString) {
   const date = new Date(dateString);
-  //
+
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const monthShort = date.toLocaleString("en-US", { month: "short" });
-  const year = date.toLocaleString("en-US", { year: "numeric" });
-  //
+  const year = date.getFullYear();
+  const time = date.toLocaleString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true });
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours > 12 ? "pm" : "am";
+
   return {
     date: `${day}-${monthShort}-${year}`,
     monthDate: `${monthShort} ${parseInt(day)}`,
     year,
     monthShort,
     day: parseInt(day),
+    time,
+    hours,
+    minutes,
+    ampm,
   };
 }
 
