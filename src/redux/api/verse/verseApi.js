cebc0fdb98c9fc53430576d76491b129cce45ba8
Theo Ojochegbe <symplytheo@gmail.com>
2024-04-03 06:30:10 +0100

    feat: add getRandomVerse

--- src/redux/api/verse/verseApi.js
+++ src/redux/api/verse/verseApi.js
@@ -4,14 +4,18 @@ const verseApi = api.injectEndpoints({
   endpoints: (build) => ({
     getAllVerses: build.query({
       query: () => ({ url: "/verses", method: "GET" }),
-      transformResponse: (verses, meta) => {
+      transformResponse: (verses) => {
         return verses.data;
       },
       transformErrorResponse: (response) => response.data?.message,
     }),
+    getRandomVerse: build.query({
+      query: () => "/verses/get-random-verse",
+      transformResponse: (verse) => verse?.data,
+    }),
   }),
 });
 
-export const { useGetAllVersesQuery } = verseApi;
+export const { useGetAllVersesQuery, useGetRandomVerseQuery } = verseApi;
 
 export default verseApi;
