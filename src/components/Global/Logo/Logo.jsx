099129dbb6d545e20a255c39f6d233458e479e15
peter michael <enitanpeters28@gmail.com>
2024-04-15 12:28:54 +0100

    logo

--- src/components/Global/Logo/Logo.jsx
+++ src/components/Global/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 const Logo = ({ className }) => {
-  return <img src="/CMDALogo.svg" alt="CMDA Nigeria" className={"h-14 " + className} />;
+  return <img src="/CMDALogo.svg" alt="CMDA Nigeria" className={"h-14 w-20" + className} />;
 };
 
 export default Logo;
