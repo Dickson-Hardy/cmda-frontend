8ec8684a11879e84d54d3c19930e4b16e2a65203
Theo Ojochegbe <symplytheo@gmail.com>
2024-04-16 14:04:29 +0100

    chore: correct spelling

--- src/components/DashboardComponents/ProfileTabContents/TrainingRecord.jsx
+++ src/components/DashboardComponents/ProfileTabContents/TrainingRecord.jsx
@@ -1,7 +1,7 @@
 const ProfileTabTrainingRecord = () => {
   return (
     <div className="w-full  px-4 py-4">
-      <h3 className="text-base font-bold mb-2">Trainings Record</h3>
+      <h3 className="text-base font-bold mb-2">Training Records</h3>
       <div className="space-y-4 py-12 text-center text-gray-dark">No training attended yet</div>
     </div>
   );
